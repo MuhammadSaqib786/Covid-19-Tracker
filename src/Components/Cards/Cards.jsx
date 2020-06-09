af0f954a2b0c65fffdac15ed16c092b98ab6fe15
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-09 02:15:35 -0700

    changed code

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -13,7 +13,7 @@ const Cards = ({data :{confirmed,recovered,deaths,lastUpdate}}) =>
     return(
         <div className={ styles.container}>
             <Grid container spacing={3} justify = "center">
-                <h2  className={ styles.author}>COVID-19 APP CREATED BY SAQIB KAYANI</h2>
+                <h2  className={ styles.author}>COVID-19 APP<br/> CREATED BY SAQIB KAYANI</h2>
             </Grid>
             <Grid container spacing={3} justify = "center">
                 <Grid item component={Card}  xs={12} md={3} className={cx(styles.card,styles.infected)}>
